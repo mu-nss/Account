b3b549b9290c6e276eba0adaafae7fa16309b3aa
mu-nss <msj20161282@gmail.com>
2024-11-26 20:17:30 +0900

    잔액사용기능구현

--- src/main/java/com/example/account/domain/Account.java
+++ src/main/java/com/example/account/domain/Account.java
@@ -1,6 +1,8 @@
 package com.example.account.domain;
 
+import com.example.account.exception.AccountException;
 import com.example.account.type.AccountStatus;
+import com.example.account.type.ErrorCode;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -36,4 +38,13 @@ public class Account {
     private LocalDateTime createdAt;
     @LastModifiedDate
     private LocalDateTime updatedAt;
+
+    // 중요한 데이터 변경을 엔티티 안에서 처리
+    public void useBalance(Long amount) {
+        if (amount > balance) {
+            throw new AccountException(ErrorCode.AMOUNT_EXCEED_BALANCE);
+        }
+
+        balance -= amount;
+    }
 }
