80863c35b1a4d6bf85c1e6061f13a38956011ad3
mu-nss <msj20161282@gmail.com>
2024-11-28 18:17:39 +0900

    중복거래방지기능구현및테스트

--- src/main/java/com/example/account/controller/AccountController.java
+++ src/main/java/com/example/account/controller/AccountController.java
@@ -5,7 +5,6 @@ import com.example.account.dto.AccountInfo;
 import com.example.account.dto.CreateAccount;
 import com.example.account.dto.DeleteAccount;
 import com.example.account.service.AccountService;
-import com.example.account.service.RedisTestService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,7 +16,7 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class AccountController {
     private final AccountService accountService;
-    private final RedisTestService redisTestService;
+//    private final LockService redisTestService;
 
     // 계좌생성
     @PostMapping("/account")
@@ -60,10 +59,10 @@ public class AccountController {
                 .collect(Collectors.toList());
     }
 
-    @GetMapping("/get-lock")
-    public String getLock() {
-        return redisTestService.getLock();
-    }
+//    @GetMapping("/get-lock")
+//    public String getLock() {
+//        return redisTestService.getLock();
+//    }
 
     @GetMapping("/account/{id}")
     public Account getAccount(
--- src/main/java/com/example/account/controller/TransactionController.java
+++ src/main/java/com/example/account/controller/TransactionController.java
@@ -1,5 +1,6 @@
 package com.example.account.controller;
 
+import com.example.account.aop.AccountLock;
 import com.example.account.dto.CancelBalance;
 import com.example.account.dto.QueryTransactionResponse;
 import com.example.account.dto.UseBalance;
@@ -26,8 +27,10 @@ public class TransactionController {
     @PostMapping("/transaction/use")
     public UseBalance.Response useBalance(
             @Valid @RequestBody UseBalance.Request request
-    ) {
+    ) throws InterruptedException {
         try {
+            Thread.sleep(5000L);
+
             return UseBalance.Response.from(
                     transactionService.useBalance(
                             request.getUserId(),
@@ -47,6 +50,7 @@ public class TransactionController {
     }
 
     @PostMapping("/transaction/cancel")
+    @AccountLock
     public CancelBalance.Response cancelBalance(
             @Valid @RequestBody CancelBalance.Request request
     ) {
